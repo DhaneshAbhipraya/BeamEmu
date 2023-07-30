d2c87d94deeab2763f58471d1e742c4efe67945e
DhaneshAbhipraya <dhanesh.abhipraya@gmail.com>
2023-07-30 14:23:45 +0700

    NOPE

--- nope/assembler.py
+++ nope/assembler.py
@@ -7,9 +7,11 @@ def main():
     args = parseargs()
     try:
         nargs = normargs(args)
-        print(f"Normalize args: {nargs}")
+        if args[2]:
+            print(f"Normalize args: {nargs}")
         aargs = validateargs(nargs)
-        print(f"Validated args: {aargs}")
+        if args[2]:
+            print(f"Validated args: {aargs}")
     except Exception as e:
         print("Error when parsing args." +
               (" Rerun using `-v` to see what was wrong" if not args[2] else ""))
@@ -27,10 +29,12 @@ def parseargs():
         '-of', help="Output file after being assembled; should be a `.bin`, or `!` to use the same file with the .bin file extension [Default: `!`]", dest="_of_", default="!")
     parser.add_argument(
         '-v', '--verbose', help="Output verbose info", dest="_verbose_", action="store_true")
+    parser.add_argument(
+        '-vs', '--verbose-short', help="Output short verbose info", dest="_verbose_short_", action="store_true")
 
     args = parser.parse_args()
 
-    return [args._if_, args._of_, args._verbose_]
+    return [args._if_, args._of_, args._verbose_ or args._verbose_short_, args._verbose_short_]
 
 
 def normargs(args):
@@ -57,24 +61,24 @@ def validateargs(args):
     return args
 
 
-def start_asm(ifile, ofile, verbose):
+def start_asm(ifile, ofile, verbose, verbose_short):
     print(f"Assembling {path.basename(ifile)} to {path.basename(ofile)}...")
     start = pc()
     if verbose:
-        print("Creating translation tables...")
+        print("create trtab"if verbose_short else"Creating translation tables...", end=' | ')
     if verbose:
-        print("Creating translation table for instructions...")
+        print("create trtabinst"if verbose_short else"Creating translation table for instructions...", end=' | ')
     if verbose:
-        print("Creating translation table for instructions > constants...")
+        print("create trtabinst>const"if verbose_short else"Creating translation table for instructions > constants...", end=' | ')
     LABEL = 0
     VALUE = 1
     REGISTER = 2
     if verbose:
-        print("Creating translation table for instructions > table...")
+        print("create trtabinst>table"if verbose_short else"Creating translation table for instructions > table...", end=' | ')
     trtabinst = {
         "nop": [b'\x00', []],
         "jmp": [b'\x01', [LABEL]],
-        "mov": [b'\x02', [VALUE, REGISTER]],
+        "mov": [b'\x02', [REGISTER, VALUE]],
         "inc": [b'\x03', [REGISTER]],
         "dec": [b'\x04', [REGISTER]],
         "add": [b'\x05', [REGISTER, VALUE]],
@@ -92,28 +96,63 @@ def start_asm(ifile, ofile, verbose):
         "jng": [b'\x11', [LABEL]],
         "out": [b'\x12', [VALUE]],
     }
+    if verbose:
+        print("create trtabreg"if verbose_short else"Creating translation table for registers...", end=' | ')
+    trtabreg = {
+        "eax": 0x00,
+        "ebx": 0x01,
+        "ecx": 0x02,
+        "edx": 0x02,
+    }
+    if verbose:
+        print("create labels"if verbose_short else"Creating label map...", end=' | ')
+    labels = {}
 
     out = b''
 
     with open(ifile, 'r') as i:
         if verbose:
-            print("Reading source...")
+            print("read source"if verbose_short else"Reading source...", end=' | ')
         source = i.read()
 
-    for line in source.split('\n'):
+    for _line in enumerate(source.split('\n')):
+        line = _line[1].strip()
         tokens = line.split()
+        if tokens[0].endswith(':'):
+            labels.update({tokens[0].removesuffix(':'): _line[0]})
+    for _line in source.split('\n'):
+        line = _line.strip()
+        tokens = line.split()
+        if tokens[0].endswith(':'):
+            continue
+        if len(tokens) < 1:
+            continue
         out += trtabinst[tokens[0]][0]
         args = trtabinst[tokens[0]][1]
         for arg in enumerate(args):
+            toadd = None
             if arg[1] == VALUE:
-                out += int(tokens[arg[1]]).to_bytes()
+                if tokens[arg[0]+1] in trtabreg:
+                    toadd = trtabreg[tokens[arg[0]+1]].to_bytes(1, "little")
+                else:
+                    lit = tokens[arg[0]+1]
+                    if tokens[arg[0]+1].startswith('0x'):
+                        lit = tokens[arg[0]+1].removeprefix('0x')
+                    toadd = int(lit).to_bytes(2, "little")
+                    if tokens[arg[0]+1].startswith('0x'):
+                        int(lit).real
+            if arg[1] == LABEL:
+                toadd = int(labels[tokens[arg[0]+1]]).to_bytes(2, 'little')
+
+            if toadd:
+                out += toadd
 
     with open(ofile, 'wb') as o:
         if verbose:
-            print("Writing binary...")
+            print("write bin"if verbose_short else"Writing binary...", end=' | ')
         o.write(out)
 
-    print("Assembled in {:.3}ms".format((pc() - start) * 1000))
+    print("done\nAssembled in {:.3}ms".format((pc() - start) * 1000))
 
 
 if __name__ == "__main__":
---
+++ nope/src/init.asm
@@ -0,0 +1,9 @@
+    jmp nothingtoseeheremovealong
+    mov eax 18533
+    mov ebx 27756
+    mov ecx 28460
+    mov edx 8279
+    add ecx 28530
+    add eax 27748
+    add eax 8448
+nothingtoseeheremovealong:
